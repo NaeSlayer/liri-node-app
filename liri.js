15f95882560eff82ad47160d8a428f06b9bec4ad
lanae <rlfoard@gmail.com>
2018-03-28 15:57:56 -0700

    minor progress

--- liri.js
+++ liri.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-var keys = require("keys.js");
+var keys = require("./keys.js");
 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
@@ -27,4 +27,21 @@ switch (command) {
     case "do-what-it-says":
         doTheThing();
 
+}
+
+function omdb() {
+    var queryUrl = "http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=trilogy";
+
+    console.log(queryUrl);
+
+    request(queryUrl, function (error, response, body) {
+        // If the request is successful
+        if (!error && response.statusCode === 200) {
+
+            // Parse the body of the site and recover just the imdbRating
+            // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+            console.log("Release Year: " + JSON.parse(body).Year);
+        }
+
+    })
 }
