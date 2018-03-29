90c07d95b25ccd93e8fefe86da95a93daec17bb8
lanae <rlfoard@gmail.com>
2018-03-29 15:37:50 -0700

    functional

--- liri.js
+++ liri.js
@@ -1,9 +1,13 @@
 require("dotenv").config();
 
 var keys = require("./keys.js");
+var Spotify = require("node-spotify-api");
+var Twitter = require("twitter");
+
 
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+
 
 var request = require("request");
 
@@ -17,11 +21,11 @@ switch (command) {
         break;
 
     case "spotify-this-song":
-        spotify();
+        spotifyThis(input);
         break;
 
     case "movie-this":
-        omdb();
+        omdb(input);
         break;
 
     case "do-what-it-says":
@@ -29,18 +33,67 @@ switch (command) {
 
 }
 
-function omdb() {
+function tweet() {
+    var params = {
+        screen_name: 'LanaeSlayer'
+    };
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error) {
+            for (var i = 0; i < tweets.length; i++) {
+
+
+                console.log("");
+                console.log(tweets[i].created_at);
+                console.log(tweets[i].text);
+                console.log("");
+                console.log("<-------------------------------------------->");
+            }
+        }
+    });
+
+}
+
+
+function spotifyThis(input) {
+    console.log("hello");
+
+
+    spotify.search({
+        type: 'track',
+        query: input,
+    }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        console.log("<-------------------------------------------->");
+        console.log("Artist: " + data.tracks.items[0].artists[0].name);
+        console.log("Song Name: " + data.tracks.items[0].name);
+        console.log("Album Name: " + data.tracks.items[0].album.name);
+        console.log("Preview Link: " + data.tracks.items[0].preview_url);
+        console.log("< -------------------------------------------->");;
+    });
+}
+
+function omdb(input) {
+    input = input.replace(' ', '+');
     var queryUrl = "http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=trilogy";
 
     console.log(queryUrl);
 
     request(queryUrl, function (error, response, body) {
+        var data = JSON.parse(body)
         // If the request is successful
         if (!error && response.statusCode === 200) {
 
-            // Parse the body of the site and recover just the imdbRating
-            // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-            console.log("Release Year: " + JSON.parse(body).Year);
+            console.log("Title: " + data.Title)
+            console.log("Release Year: " + data.Year);
+            console.log("IMDB Rating: " + data.imdbRating);
+            console.log("Rotten Tomatoes Rating: " + data.Ratings[1].Value);
+            console.log("Country: " + data.Country);
+            console.log("Language: " + data.Language);
+            console.log("Plot: " + data.Plot);
+            console.log("Actors: " + data.Actors);
+
         }
 
     })
