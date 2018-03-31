5786f9138533c904b282e57b7910cee665edfd45
lanae <rlfoard@gmail.com>
2018-03-30 20:17:02 -0700

    fully functional

--- liri.js
+++ liri.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-
+var fs = require("fs");
 var keys = require("./keys.js");
 var Spotify = require("node-spotify-api");
 var Twitter = require("twitter");
@@ -39,6 +39,7 @@ function tweet() {
     };
     client.get('statuses/user_timeline', params, function (error, tweets, response) {
         if (!error) {
+            data = [];
             for (var i = 0; i < tweets.length; i++) {
 
 
@@ -47,7 +48,14 @@ function tweet() {
                 console.log(tweets[i].text);
                 console.log("");
                 console.log("<-------------------------------------------->");
+                data.push({
+                    time: tweets[i].created_at,
+                    text: tweets[i].text
+                })
+
             }
+            logData(data)
+
         }
     });
 
@@ -55,8 +63,9 @@ function tweet() {
 
 
 function spotifyThis(input) {
-    console.log("hello");
-
+    if (!input) {
+        input = "All that she wants";
+    }
 
     spotify.search({
         type: 'track',
@@ -65,16 +74,32 @@ function spotifyThis(input) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
-        console.log("<-------------------------------------------->");
-        console.log("Artist: " + data.tracks.items[0].artists[0].name);
-        console.log("Song Name: " + data.tracks.items[0].name);
-        console.log("Album Name: " + data.tracks.items[0].album.name);
-        console.log("Preview Link: " + data.tracks.items[0].preview_url);
-        console.log("< -------------------------------------------->");;
+        var songs = [];
+        for (var i = 0; i < 4; i++) {
+            var song = data.tracks.items[i]
+
+            songs.push({
+                "Artist": song.artists[0].name,
+                "Song Name": song.name,
+                "Album Name": song.album.name,
+                "Preview Link": song.preview_url
+            })
+            // console.log(data.tracks);
+            console.log("<-------------------------------------------->");
+            console.log("Artist: " + song.artists[0].name);
+            console.log("Song Name: " + song.name);
+            console.log("Album Name: " + song.album.name);
+            console.log("Preview Link: " + song.preview_url);
+            console.log("< -------------------------------------------->");
+        }
+        logData(songs);
     });
 }
 
 function omdb(input) {
+    if (!input) {
+        input = "Mr. Nobody";
+    }
     input = input.replace(' ', '+');
     var queryUrl = "http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=trilogy";
 
@@ -82,8 +107,21 @@ function omdb(input) {
 
     request(queryUrl, function (error, response, body) {
         var data = JSON.parse(body)
+        var movie = [];
         // If the request is successful
         if (!error && response.statusCode === 200) {
+
+            movie.push({
+                "Title": data.Title,
+                "Release Year": data.Year,
+                "IMDB Rating": data.imdbRating,
+                "Rotten Tomatoes Rating": data.Ratings[1].Value,
+                "Country": data.Country,
+                "Language": data.Language,
+                "Plot": data.Plot,
+                "Actors": data.Actors
+
+            })
 
             console.log("Title: " + data.Title)
             console.log("Release Year: " + data.Year);
@@ -94,7 +132,37 @@ function omdb(input) {
             console.log("Plot: " + data.Plot);
             console.log("Actors: " + data.Actors);
 
+            logData(movie);
+
         }
 
     })
+}
+
+function doTheThing() {
+    fs.readFile("random.txt", "utf8", function (error, data) {
+
+        // If the code experiences any errors it will log the error to the console.
+        if (error) {
+            return console.log(error);
+        }
+
+        // Then split it by commas (to make it more readable)
+        var dataArr = data.split(",");
+
+        spotifyThis(dataArr[1])
+
+
+
+    });
+}
+
+function logData(data) {
+    fs.appendFile('log.txt', "\n" + JSON.stringify(data), function (error) {
+        if (error) {
+            throw error;
+        }
+        console.log('The "data" was appended to file!');
+    });
+
 }
