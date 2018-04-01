8a780c658d12a61ce0686499b55a74e1e1fbc5cb
lanae <rlfoard@gmail.com>
2018-03-31 23:26:43 -0700

    added pseudocode

--- liri.js
+++ liri.js
@@ -1,3 +1,5 @@
+// setting up require functions for node.js packages
+
 require("dotenv").config();
 var fs = require("fs");
 var keys = require("./keys.js");
@@ -5,14 +7,17 @@ var Spotify = require("node-spotify-api");
 var Twitter = require("twitter");
 var request = require("request");
 
+// keys required for spotify and twitter
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
+// these variables store in user inputs
 var command = process.argv[2];
 var input = process.argv[3];
 
+// this message displays if <node liri.js> is entered into the command line
 if (!command) {
-    console.log("Welcome to LIRI! \nLIRI is a Language Interpretation and Recognition Interface. \nLIRI will display tweets or search for movies or songs. \nEnter 'my-tweets' to display Tweets \nEnter'spotify-this-song' and 'song name' to display songs \nEnter 'movie-this' and 'movie-title' to display movies")
+    console.log("Welcome to LIRI! \nLIRI is a Language Interpretation and Recognition Interface. \nLIRI will display tweets or search for movies or songs. \nEnter <node liri.js my-tweets> to display Tweets \nEnter <node liri.js spotify-this-song song-name> to display songs \nEnter <node liri.js movie-this movie-title> to display movies")
 
 }
 
@@ -35,14 +40,16 @@ switch (command) {
         doTheThing();
 
 }
-
+// This function displays the last 20 tweets for the screen_name entered
 function tweet() {
     var params = {
         screen_name: 'LanaeSlayer'
     };
     client.get('statuses/user_timeline', params, function (error, tweets, response) {
         if (!error) {
+            // This array will hold the data that will be appended to log.txt
             data = [];
+
             for (var i = 0; i < tweets.length; i++) {
 
 
@@ -64,7 +71,7 @@ function tweet() {
 
 }
 
-
+// This function searches for the song name entered
 function spotifyThis(input) {
     if (!input) {
         input = "All that she wants";
@@ -77,10 +84,11 @@ function spotifyThis(input) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
+        // This array will hold the data that will be appended to log.txt
         var songs = [];
         for (var i = 0; i < 4; i++) {
             var song = data.tracks.items[i]
-
+            // this pushes the song information to the songs array
             songs.push({
                 "Artist": song.artists[0].name,
                 "Song Name": song.name,
@@ -96,7 +104,7 @@ function spotifyThis(input) {
         logData(songs);
     });
 }
-
+// This function sends a request to the OMDB API and returnd movie information. If not movie title is entered it will return information for Mr.Nobody
 function omdb(input) {
     if (!input) {
         input = "Mr. Nobody";
@@ -139,7 +147,7 @@ function omdb(input) {
 
     })
 }
-
+// This function uses the 'fs' node package to take text from inside random.text and then use it to call one of LIRI's commands
 function doTheThing() {
     fs.readFile("random.txt", "utf8", function (error, data) {
 
@@ -157,7 +165,7 @@ function doTheThing() {
 
     });
 }
-
+// This function appends everything that is logged in the console to log.txt
 function logData(data) {
     fs.appendFile('log.txt', "\n" + JSON.stringify(data), function (error) {
         if (error) {
